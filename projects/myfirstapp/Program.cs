8be0b42872aa2a555c7dc173a14084a9b90c1f76
LVanesa10 <90993076+LVanesa10@users.noreply.github.com>
2022-08-21 23:18:03 -0500

    Add files via upload

---
+++ projects/myfirstapp/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Ingrese su nombre completo:");
+string nombre = (Console.ReadLine());
+
+Console.WriteLine("Ingrese su edad:");
+uint edad = uint.Parse(Console.ReadLine());
+
+double entrada, descuento1, descuento2, descuento3, total1, total2, total3;
+
+entrada = 15000;
+descuento1 = (15000*1);
+descuento2 = (15000*0.05);
+descuento3 = (15000*0.03);
+total1 = (entrada - descuento1);
+total2 = (entrada - descuento2);
+total3 = (entrada - descuento3);
+
+
+if(edad<4){
+    Console.WriteLine("Usted " + nombre + " con la edad de " + edad + " años. El precio de la entrada es de: " + entrada + " usted obtuvo un total de descuento de: " + descuento1 + " el valor total a pagar es de: " + total1);
+}
+else if(edad>= 4 && edad<=18){
+    Console.WriteLine("Usted " + nombre + " con la edad de " + edad + " años. El precio de su entrada es de: " + entrada + " usted obtuvo un total de descuento de: " + descuento2 + " el valor total a pagar es de: " + total2);
+}
+else{
+    Console.WriteLine("Usted " + nombre + " con la edad de " + edad + " años. El precio de su entrada es de: " + entrada + " usted obtuvo un total de descuento de: " + descuento3 + " el valor total a pagar es de: " + total3);
+}
---
+++ projects/myfirstapp/Punto1/README.md
@@ -0,0 +1,6 @@
+# https://git-scm.com/docs/gitignore
+# https://help.github.com/articles/ignoring-files
+# Example .gitignore files: https://github.com/github/gitignore
+/.vscode/
+git config --global user.email SU-CORREO-ELECTRONICO
+git config --global user.name "SU-NOMBRE-DE-USUARIO-DE-GITUHUB"
